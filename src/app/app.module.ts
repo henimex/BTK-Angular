fc3b174816181a6fe2534f89d52ded7b5675f3b5
J.Henimex <J.Henimex@IT-01>
2021-03-09 12:07:47 +0300

    SearchBox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CategoryComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product-filter.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from './product';
+
+@Pipe({
+  name: 'productFilter'
+})
+export class ProductFilterPipe implements PipeTransform {
+
+  transform(value: Product[], filterText?: string ): Product[] {
+    //filterText = filterText ? filterText.toLocaleLowerCase():"null"
+
+    return filterText ? value.filter((p:Product)=>p.name?.toLocaleLowerCase().indexOf(filterText.toLocaleLowerCase()) != -1):value;
+    //return filterText ? value.filter((p: Product) => p.name?.toLocaleLowerCase().indexOf(filterText.toLocaleLowerCase()) != -1) : value;
+  }
+
+}
